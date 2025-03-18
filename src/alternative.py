4bde51f9d7685305871ead9aa2d21e6ee397d036
Yauheni <160595384+tpyauheni@users.noreply.github.com>
2025-03-18 20:58:57 +0300

    Increased reliability of TCP

--- src/alternative.py
+++ src/alternative.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, final
 
 import ggwave
 import pyaudio
@@ -36,12 +36,14 @@ class AlternativeStream(BufferedStream):
     output_lock: threading.Lock
     reader: threading.Thread
     writer: threading.Thread
+    output_chunk_bytes: int
 
     def __init__(self, turn_write: bool) -> None:
         self.transformer = ggwave.init()
         self.ctx = pyaudio.PyAudio()
         self.input_stream = self.ctx.open(format=pyaudio.paFloat32, channels=1, rate=48_000, input=True, frames_per_buffer=1024)
-        self.output_stream = self.ctx.open(format=pyaudio.paFloat32, channels=1, rate=48_000, output=True, frames_per_buffer=4096)
+        self.output_chunk_bytes = 1024 * 4
+        self.output_stream = self.ctx.open(format=pyaudio.paFloat32, channels=1, rate=48_000, output=True, frames_per_buffer=self.output_chunk_bytes // 4)
         self.input_lock = threading.Lock()
         self.output_lock = threading.Lock()
         self.reader = threading.Thread(target=self._read_loop)
@@ -70,18 +72,30 @@ class AlternativeStream(BufferedStream):
         except OSError as exc:
             print(exc)
 
-    def _write_loop(self) -> None:
-        while True:
-            if not self._turn_write:
-                time.sleep(0.01)
+    def _try_write(self) -> bool:
+        if not self._turn_write:
+            return False
 
-            if len(self.output_buffer) == 0:
-                time.sleep(0.01)
-                continue
+        if len(self.output_buffer) == 0:
+            return False
 
-            with self.output_lock:
+        if self.output_lock.acquire(blocking=False):
+            try:
                 data: bytes = self.output_buffer.pop()
                 self._write(data)
+            finally:
+                self.output_lock.release()
+
+            return True
+
+        return False
+
+    def _write_loop(self) -> None:
+        silence: bytes = b'\0' * self.output_chunk_bytes
+
+        while True:
+            self._try_write()
+            self.output_stream.write(silence)
 
     def _write(self, data: bytes) -> None:
         frames: bytes = ggwave.encode(GgwObject(data), protocolId=5, volume=100)
@@ -109,7 +123,7 @@ class ReliableTransceiver:
     SYN: int = 1
     # Acknowledged (from TCP)
     ACK: int = 2
-    # Retry
+    # Retry (unused)
     RTR: int = 4
 
     session_started: bool = False
@@ -125,6 +139,7 @@ class ReliableTransceiver:
         size: int = len(data)
         start_time: float = time.time()
         result: bytes = bytes()
+        self.stream.turn_read()
 
         while True:
             time_now: float = time.time()
@@ -147,7 +162,13 @@ class ReliableTransceiver:
 
             time.sleep(precision)
 
-    def read(self, size: int, abort_timeout: float = 15.0, precision: float = 0.01) -> bytes:
+    def read(
+            self,
+            size: int,
+            abort_timeout: float = 15.0,
+            send_ack: bool = True,
+            precision: float = 0.01,
+    ) -> bytes | tuple[bytes, bytes]:
         if size <= 0:
             raise ValueError('Attempted to read data with non-positive size from a stream')
 
@@ -191,12 +212,16 @@ class ReliableTransceiver:
                     time.sleep(0.5)
                     start_time += 0.5
                     self.stream.clear_input_buffer()
+                    # We are sending ACK even if `send_ack` is False as it only determines
+                    # such behavior if `batch_id` == `self.last_received_packet`.
                     self.stream.turn_write()
                     self.stream.write(struct.pack('<BB', batch_id, self.ACK))
                     self.stream.turn_read()
                     continue
 
                 break
+
+        ack_packet: bytes = struct.pack('<BB', batch_id, self.ACK)
 
         while size > 0:
             time_now: float = time.time()
@@ -213,14 +238,19 @@ class ReliableTransceiver:
                 print('Returning more bytes that requested!')
 
             if size <= 0:
-                self.stream.turn_write()
-                self.stream.write(struct.pack('<BB', batch_id, self.ACK))
-                self.stream.turn_read()
+                if send_ack:
+                    self.stream.turn_write()
+                    self.stream.write(ack_packet)
+                    self.stream.turn_read()
                 break
 
             time.sleep(precision)
 
-        return result
+        if send_ack:
+            return result
+
+        print(ack_packet, result, type(ack_packet), type(result))
+        return ack_packet, result
 
     def write(self, data: bytes, resend_timeout: float = 1.0, abort_retries: int = 5, precision: float = 0.01) -> None:
         self.last_sent_packet += 1
@@ -300,17 +330,21 @@ class ReliableTransceiver:
             retries: int = 3
 
             while retries >= 0:
+                print('Iter with retries', retries)
+                self.stream.turn_write()
                 self.stream.write(struct.pack('<B', self.SYN))
                 print('Sent `SYN`')
                 self.stream.turn_read()
 
                 try:
-                    data: bytes = self.read(1, abort_timeout=1.5)
+                    ack, data = self.read(1, abort_timeout=2.5, send_ack=False)
                 except ConnectionAbortedError:
                     print('Connection aborted')
                     retries = -1
                     break
 
+                assert isinstance(ack, bytes)
+                assert isinstance(data, bytes)
                 # print(data)
                 response: int = struct.unpack('<B', data)[0]
 
@@ -318,9 +352,13 @@ class ReliableTransceiver:
                     print('Another peer responded with something different than `SYN|ACK`:', response)
                     time.sleep(reconnect_interval)
                     retries -= 1
+                    print('Retries:', retries)
                     continue
 
                 print('Got `SYN|ACK`')
+                self.stream.turn_write()
+                self.stream.write(ack)
+                print('Sent `ACK`')
                 break
 
             if retries >= 0:
@@ -361,11 +399,83 @@ class ReliableTransceiver:
             if retries >= 0:
                 break
 
+    def key_exchange_sender(self) -> None:
+        key_exchanger: KeyExchanger = KeyExchanger(None, None)
+        key: bytes = key_exchanger.exchange_pubkey()
+        print('Sending key: ', key)
+        self.write(key)
+        print('Sent that key')
+        their_pubkey: bytes = self.read(32)
+        print('My public key: ', key_exchanger.pubkey())
+        print('Their public key: ', their_pubkey)
+        print('My secret key: ', key_exchanger.seckey())
+        session_key: SymmetricKey = key_exchanger.get_symkey(their_pubkey)
+        print('Session key: ', session_key.key_ref()[0])
+
+        ciphertext: bytes = session_key.encrypt(b'Hello')
+        print('Sending cipher: ', ciphertext)
+        self.write(ciphertext)
+
+        # Encrypted Client Hello
+        ech: bytes = self.read(8 + 2)
+        decrypted: bytes = session_key.decrypt(ech)
+        assert decrypted == b'Hi'
+
+        ciphertext = session_key.encrypt(struct.pack('<B', self.ACK))
+        print('Sending cipher: ', ciphertext)
+        self.write(ciphertext)
+
+        print('Connection established')
+
+        # TODO
+
+        session_key.dispose()
+        self.stream.dispose()
+        print('Connection finished')
+
+    def key_exchange_receiver(self) -> None:
+        key_exchanger: KeyExchanger = KeyExchanger(None, None)
+        their_pubkey: bytes = self.read(32)
+        print('Their public key: ', their_pubkey)
+        session_key: SymmetricKey = key_exchanger.get_symkey(their_pubkey, dispose=False)
+        print('My public key: ', key_exchanger.pubkey())
+        print('My secret key: ', key_exchanger.seckey())
+        print('Session key: ', session_key.key_ref()[0])
+        key: bytes = key_exchanger.exchange_pubkey()
+        print('Sender\'s key: ', key)
+        self.write(key)
+        print('Sent key', flush=True)
+
+        # Encrypted Client Hello
+        print('Reading ECH')
+        ech: bytes = self.read(8 + 5)
+        decrypted: bytes = session_key.decrypt(ech)
+        assert decrypted == b'Hello'
+
+        ciphertext: bytes = session_key.encrypt(b'Hi')
+        print('Sending cipher: ', ciphertext)
+        self.write(ciphertext)
+
+        print('did that')
+        print('almost there', flush=True)
+        plaintext: bytes = session_key.decrypt(self.read(8 + 1))
+        print('Should be ACK:', plaintext)
+        assert struct.unpack('<B', plaintext)[0] == self.ACK
+
+        print('Connection established')
+
+        # TODO
+
+        session_key.dispose()
+        self.stream.dispose()
+        print('Connection finished')
+
     def connect(self, is_sender: bool) -> None:
         while True:
             print(f'Connecting as {['receiver', 'sender'][is_sender]}...')
             (self.connect_init_sender if is_sender else self.connect_init_receiver)()
-            print('Initial (unencrypted) connection established')
+            print('Initial (unencrypted) connection established, exchanging keys...')
+            (self.key_exchange_sender if is_sender else self.key_exchange_receiver)()
 
             try:
                 self.read(1)
@@ -382,27 +492,9 @@ def sender() -> None:
         ggwave.disableLog()
 
     stream: AlternativeStream = AlternativeStream(True)
-    ggwave.txToggleProtocol(5, 1)
-    ggwave.rxToggleProtocol(1, 1)
     transceiver: ReliableTransceiver = ReliableTransceiver(stream)
     transceiver.connect(True)
     return
-
-    while True:
-        stream.clear_input_buffer()
-        stream.write(struct.pack('<B', SYN))
-        stream.turn()
-        data: bytes = stream.read(1)
-        stream.turn()
-
-        if struct.unpack('<B', data)[0] != SYN | ACK:
-            print('Another peer responded with something different than `SYN|ACK`')
-            time.sleep(3.0)
-            continue
-
-        stream.write(struct.pack('<B', ACK))
-        stream.turn()
-        break
 
     print('Initialization sequence complete, exchanging keys...')
     key_exchanger: KeyExchanger = KeyExchanger(None, None)
@@ -462,30 +554,9 @@ def receiver() -> None:
         ggwave.disableLog()
 
     stream: AlternativeStream = AlternativeStream(False)
-    ggwave.rxToggleProtocol(5, 1)
-    ggwave.txToggleProtocol(1, 1)
     transceiver: ReliableTransceiver = ReliableTransceiver(stream)
     transceiver.connect(False)
     return
-
-    while True:
-        stream.clear_input_buffer()
-
-        if struct.unpack('<B', stream.read(1))[0] != SYN:
-            print('!SYN')
-            continue
-
-        stream.turn()
-        stream.write(struct.pack('<B', SYN | ACK))
-        stream.turn()
-
-        if struct.unpack('<B', stream.read(1))[0] != ACK:
-            print('Another peer responded with something different than `ACK`')
-            time.sleep(3.0)
-            continue
-
-        # stream.write(struct.pack('<B', ACK))
-        break
 
     stream.turn()
     print('Initialization sequence complete, exchanging keys...')
